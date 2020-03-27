63eda7e1023055e278b78fb8cc1eee101e73f0d1
tortique <alexeev-eugene@outlook.com>
2020-03-27 15:21:04 +0300

    added gates connecting by ids

--- include/sim/sim.h
+++ include/sim/sim.h
@@ -38,29 +38,50 @@ public:
         }
     }
     void connect_gates(size_t id1, size_t id2){
-        auto it1 = elements.end();
-        auto it2 = elements.end();
+        std::shared_ptr<gate> gt1, gt2;
         for(auto it = elements.begin(); it < elements.end(); it++){
-            if(it->get()->get_id() == id1 && 
-                it1 == elements.end())
-            {
-                it1 = it;
-                continue;
+            auto &gates_in = (*it)->access_ins();
+            for(auto gt_in = gates_in.begin(); gt_in != gates_in.end(); gt_in++){
+                if(gt_in->get()->get_id() == id1 && !gt1){
+                    gt1 = *gt_in;
+                    continue;
+                }
+                if(gt_in->get()->get_id() == id2 && !gt2){
+                    gt2 = *gt_in;
+                    break;
+                }
             }
-            if(it->get()->get_id() == id2 && 
-                it2 == elements.end())
-            {
-                it2 = it;
-                break;
+            auto &gates_out = (*it)->access_ins();
+            for(auto gt_out = gates_out.begin(); gt_out != gates_out.end(); gt_out++){
+                if(gt_out->get()->get_id() == id1 && !gt1){
+                    gt1 = *gt_out;
+                    continue;
+                }
+                if(gt_out->get()->get_id() == id2 && !gt2){
+                    gt2 = *gt_out;
+                    break;
+                }
             }
         }
-        if(it1 == elements.end()){
+        if(!gt1){
             auto mes = "no elements with id "+std::to_string(id1);
             throw std::runtime_error(mes);
         }
-        if(it2 == elements.end()){
+        if(!gt2){
             auto mes = "no elements with id "+std::to_string(id2);
             throw std::runtime_error(mes);
+        }
+        auto cast_in_1 = std::dynamic_pointer_cast<gate_in>(gt1);
+        auto cast_out_2 = std::dynamic_pointer_cast<gate_out>(gt2);
+        if(cast_in_1 && cast_out_2){
+            cast_out_2->tie_input(cast_in_1);
+            return;
+        }
+        auto cast_out_1 = std::dynamic_pointer_cast<gate_out>(gt1);
+        auto cast_in_2 = std::dynamic_pointer_cast<gate_in>(gt2);
+        if(cast_out_1 && cast_in_2){
+            cast_out_1->tie_input(cast_in_2);
+            return;
         }
     }
 };
